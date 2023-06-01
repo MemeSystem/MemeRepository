9c642a342b8d36a97eada60f72c6b7e023618004
Pelmen <@105-04>
2023-06-01 15:09:49 +0300

    все работает, ехехехехе

--- Pages/AddStory.xaml.cs
+++ Pages/AddStory.xaml.cs
@@ -12,10 +12,19 @@
 
         private void AddHistory_Click(object sender, RoutedEventArgs e)
         {
+            int count = 0;
             User? currentUser = (User?)Application.Current.Properties["CurrentUser"];
+            using (StreamReader sw = new StreamReader(Properties.Resources.HistoryPath))
+            {
+                while (!sw.EndOfStream)
+                {
+                    sw.ReadLine();
+                    count++;
+                }
+            }
             using (StreamWriter sw = new StreamWriter(Properties.Resources.HistoryPath, true, Encoding.UTF8))
             {
-                sw.WriteLine($"{Properties.Resources.HistoryPath.Length/12+1};{currentUser.UserName};{NameHistory.Text};{TagHistory.Text};{TextHistory.Text};{0}");
+                sw.WriteLine($"{count+1};{currentUser.UserName};{NameHistory.Text};{TagHistory.Text};{TextHistory.Text};{0}");
             }
             MessageBox.Show("Ваша история опубликована.", "Действие выполнено");
             NameHistory.Text = null;
